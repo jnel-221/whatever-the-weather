ae3be247763f22d6cb0c24b6ab8a877fec0cf73f
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-01-22 16:26:32 -0600

    updated API URLs

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 function callOpenWeather (){
     var thisCityName =$(this).data('name');
     var key = "13a8c1580f78868c2f813ca69b36b2cf"
-    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q="+thisCityName+"&units=imperial&appid="+key;
+    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q="+thisCityName+"&units=imperial&appid="+key;
     console.log(thisCityName);
     
     
@@ -25,28 +25,38 @@ function callOpenWeather (){
         url: queryURL,
         method: "GET"
       }).then(function(response){
-        //send response to renderMain & renderForecast functions
-        
+        //send response to renderDashboard function
+        renderDashboard(response);
 
         //create variables for 2nd API call
-        var lat = response.city.coord.lat;
-        var lon = response.city.coord.lon;
-        console.log(lat,lon);
-        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+key;
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+       
+        var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&appid="+key;
 
         $.ajax({
           url: uvURL,
           method: "GET"
         }).then(function(data){
-          console.log(data.value);
           //send data to renderUV function
+          renderUV(data);
       });
       
-      });
+    });
     
 
 };
 
+function renderDashboard(response){
+  $('#searchResults').empty()
+console.log(response)
+
+
+};
+
+function renderUV(data){
+console.log(data);
+};
 
 function makeButtons(cities){
  $("#btnList").empty();
@@ -74,7 +84,7 @@ $("#searchBtn").on("click", function(e){
     
     $("#input").val("");
    
-   callOpenWeather(cityName);
+   callOpenWeather(this);
    makeButtons(cities); 
 
 });
