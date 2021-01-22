564f03a4162fa20afeab31ca67164cbcf367b85d
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-01-22 14:34:27 -0600

    Openweather API function created, call successful

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 
-var key = "13a8c1580f78868c2f813ca69b36b2cf"
-var weatherURL = "api.openweathermap.org/data/2.5/forecast?q="+"{city name}"+"&units=imperial&appid="+key;
+
 //for use in calling API to get UV index.  This call should be chained off the first
-var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat="+"{lat}"+"&lon="+"{lon}"+"&appid="+key;
+// var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat="+"{lat}"+"&lon="+"{lon}"+"&appid="+key;
 
 var cities = [];
 
@@ -14,9 +13,31 @@ $(document).ready(function(){
     makeButtons(cities);
 });
 
-function makeButtons(cities){
-     $("#btnList").empty();
+    
+function callOpenWeather (){
+    var thisCityName =$(this).data('name');
+    var key = "13a8c1580f78868c2f813ca69b36b2cf"
+    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q="+thisCityName+"&units=imperial&appid="+key;
+    console.log(thisCityName);
+    
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
 
+        console.log(response);
+
+
+      });
+
+    
+
+}
+
+
+function makeButtons(cities){
+ $("#btnList").empty();
  for(var i = 0; i < cities.length; i++){
     var btnEl = $("<button>");
     btnEl.addClass("city text-center btn btn-secondary");
@@ -36,11 +57,15 @@ $("#searchBtn").on("click", function(e){
     var cities = JSON.parse(localStorage.getItem('buttons'))|| [];
     console.log(cities);
     
-    var btnName = $("#input").val().trim();
-    cities.push(btnName);
+    var cityName = $("#input").val().trim();
+    cities.push(cityName);
     
     $("#input").val("");
+   
+   callOpenWeather(cityName);
    makeButtons(cities); 
+
 });
+$(document).on("click", ".city", callOpenWeather);
 
 makeButtons(cities);
